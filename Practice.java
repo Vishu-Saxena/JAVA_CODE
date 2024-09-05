e2a8356a432531b56032531ac4d7ed95218098c5
Vishu_Saxena <vishusaxena3110@gmail.com>
2024-09-05 10:15:03 +0530

    revising concepts covered so far
    classes basics, array 1D ,2D , 3D , jagged array

--- Practice.java
+++ Practice.java
@@ -1,29 +1,52 @@
-class pens{
-    String color ;
-    int cost  ;
-    static String use = " likne ke liye ";
-    public static void show(pens objct){
-        System.out.println(objct.color + " : "+objct.cost + " : "+use);
+import java.util.Scanner;
+
+
+class calculator{
+    public void add(int a , int b){
+        System.out.println(a+b);
+    }
+
+    public void increment(int a){
+        System.out.println(++a);
+    }
+
+    public int increment(double a){
+        return (int) ++a;
+    }
+
+    public void add(int a , int b , int c){
+        System.out.println(a+b+c);
     }
 }
+
+
 
 public class Practice {
-    public static void main(String a[]){
-        pens obj1 = new pens();
-        obj1.color = "blue";
-        obj1.cost = 10;
-        pens obj2 = new pens();
-        obj2.color = "red";
-        obj2.cost = 15;
 
+    public static void main(String[] args) {
+        int row ;
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter number of rows : ");
+        row = s.nextInt();
 
-        pens penArray[] = new pens[2];
-        penArray[0] = obj1;
-        penArray[1] = obj2;
-        
-
-        for(pens pen : penArray){
-            pens.show(pen);
+       int num[][] = new int[row][];
+       for(int i =0 ; i< row ; i++){
+        int col;
+        System.out.println("enter the first colomn number : ");
+        col = s.nextInt();
+        num[i] = new int[col];
+       }
+       for(int i =0; i<row ; i++){
+        for(int j =0 ; j < num[i].length ; j++){
+            num[i][j] = (int)(Math.random()*10);
+            System.out.print(num[i][j] + " ");
         }
+        System.err.println();
+       }
+        
+        System.out.println("");
+       }
+
+       
+       
     }
-}
