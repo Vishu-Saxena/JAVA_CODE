9753008b98fba107221687c88797e6a532e1259c
Vishu_Saxena <vishusaxena3110@gmail.com>
2024-09-07 06:21:45 +0530

    all topics revised studied so far

--- Practice.java
+++ Practice.java
@@ -1,39 +1,23 @@
 import java.util.Scanner;
+import java.lang.Exception;
 
 
-class Caclculator{
-    public int add( int a , int b){
-        return a+b;
-    }
-    public int sub(int a , int b){
-        return a-b;
+class Calculator{
+    public int divide(int a , int b) throws ArithmeticException{
+        return a/b;
     }
 }
-
-class AdvanceCalc extends Caclculator {
-    public int multiply(int x , int y){
-        return x*y;
-    }
-    public int devide(int divident , int devisor){
-        return divident/devisor;
-    }
-}
-class ScientificCalc extends AdvanceCalc{
-    public int reminder(int x , int y){
-        return x%y;
-    }
-}
-
 
 public class Practice {
 
     public static void main(String[] args) {
-        ScientificCalc sc1 = new ScientificCalc();
-        System.out.println(sc1.add(2, 8));
-        System.out.println(sc1.sub(6, 2));
-        System.out.println( sc1.multiply(2, 4));
-        System.out.println(sc1.devide(8, 2));
-        System.out.println(sc1.reminder(9, 5));
+        Calculator c1 = new Calculator();
+        try{
+            c1.divide(6, 0);
+        }catch(ArithmeticException ae){
+            System.out.println(ae);
+        }
+        
     }
 
 }
